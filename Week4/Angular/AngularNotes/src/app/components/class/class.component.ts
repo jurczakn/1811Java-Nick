12525475fce89e0ff73914bd86c35dbc24f14c3a
@Jurczak <nickolas.jurczak@gmail.com>
2018-11-30 15:06:48 -0500

    data binding

--- Week4/Angular/AngularNotes/src/app/components/class/class.component.ts
+++ Week4/Angular/AngularNotes/src/app/components/class/class.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-class',
   templateUrl: './class.component.html',
+  //template: `<h1>Hi, this is a inline template</h1>`,
   styleUrls: ['./class.component.css']
 })
 export class ClassComponent {
--- Week4/Angular/AngularNotes/src/app/components/databinding/databinding.component.ts
+++ Week4/Angular/AngularNotes/src/app/components/databinding/databinding.component.ts
@@ -7,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabindingComponent implements OnInit {
 
+  title = `Interpolation`;
+
+  interpolationNotation = `{{}}`;
+
+  interpolation = `Welcom to the Interpolation`;
+
+  propertyBindingNotation = `[]`;
+
+  eventBindingNotation = `()`;
+
+  counter = 0;
+
+  styleState = {
+    color: `red`,
+    backgroundColor: `blue`,
+    border: `1px solid black`,
+    cursor: `pointer `,
+    margin: `2px`
+  };
+
+  changeStyles(): void {
+
+    if (this.styleState.color === `red`) {
+
+      this.styleState.color = `#fff`;
+      this.styleState.backgroundColor = `#000`;
+      this.styleState.border = `3px groove green`;
+
+    } else if (this.styleState.color === `#fff`) {
+
+      this.styleState.color = `purple`;
+      this.styleState.backgroundColor = `lime`;
+      this.styleState.border = `5px ridge cyan`;
+
+    } else {
+
+      this.styleState.color = `red`;
+      this.styleState.backgroundColor = `blue`;
+      this.styleState.border = `1px solid black`;
+
+    }
+
+  }
+
+  addToCounter(): void {
+    this.counter++;
+  }
+
   constructor() { }
 
   ngOnInit() {
