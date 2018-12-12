73c33e2c59e5b6e02c75db5b72cc9841e113b7ba
Nickolas Jurczak <nickolas.jurczak@gmail.com>
2018-12-12 11:50:27 -0500

    Add comment from website

--- staging.java
+++ staging.java
@@ -2,6 +2,7 @@ package com.revature.staging
 
 public class Staging{
 
+	//This comment was added on the github website
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
