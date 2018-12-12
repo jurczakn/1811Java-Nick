b5c2acf5ba899f17bf454e1f372349c5fe93f5e3
@Jurczak <nickolas.jurczak@gmail.com>
2018-12-12 11:51:34 -0500

    added local changes

--- staging.java
+++ staging.java
@@ -1,7 +1,7 @@
 package com.revature.staging
 
 public class Staging{
-
+	//added this comment on the git bash on local machine
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
